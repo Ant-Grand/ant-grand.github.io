b8c3df355b37e6436e7fee16abf11798431fa557
twogee <g.grigelionis@gmail.com>
2016-12-15 09:59:31 +0100

    Added latest release information

---
+++ javascripts/latest.release.js
@@ -0,0 +1,22 @@
+function GetLatestReleaseInfo(product, url) {
+    $.getJSON(url).done(function (release) {
+        var asset = release.assets[0];
+        var downloadCount = 0;
+        for (var i = 0; i < release.assets.length; i++) {
+            downloadCount += release.assets[i].download_count;
+        }
+        var oneHour = 60 * 60 * 1000;
+        var oneDay = 24 * oneHour;
+        var dateDiff = new Date() - new Date(asset.updated_at);
+        var timeAgo;
+        if (dateDiff < oneDay) {
+            timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
+        } else {
+            timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
+        }
+        var releaseInfo = release.tag_name + " was released " + timeAgo + " and downloaded " + downloadCount.toLocaleString() + " times.";
+        $(product + "-download").attr("href", asset.browser_download_url);
+        $(product + "-release-info").text(releaseInfo);
+        $(product + "-release-info").fadeIn("slow");
+    });
+}
